6afcd6457465a88e91e27db69331bcace64fd1b7
johnnypoxer7128 <johnnypoxer7128@gmail.com>
2026-07-23 08:31:20 +0200

    Fix filetool test count (8->9 with emote-resizer)

--- test/filetool.test.js
+++ test/filetool.test.js
@@ -36,7 +36,7 @@ assert.strictEqual(VKFile.bytes(5 * 1048576), "5.00 MB"); pass++;
 const IMG = require("../assets/js/tools-image.js");
 const VK = require("../data/catalog.js");
 const ids = Object.keys(IMG);
-assert.strictEqual(ids.length, 8, "8 image tools"); pass++;
+assert.strictEqual(ids.length, 9, "9 image/canvas tools (incl. emote-resizer)"); pass++;
 ids.forEach(id => {
   const t = VK.find(id);
   ok(t, `${id} in catalog`);
